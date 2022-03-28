2760c4559da088628bf631c9309a30f11061b731
figlerr <figler.renata@tmx.hu>
2022-03-28 16:42:28 +0200

    forras

--- src/test/java/hu/tmx/config/Endpoints.java
+++ src/test/java/hu/tmx/config/Endpoints.java
@@ -5,4 +5,6 @@ public interface Endpoints {
     String ALL_VIDEO_GAMES = "videogames";
     String SINGLE_VIDEO_GAME = "videogames/{id}";
     String SINGLE_AREA = "areas";
+    String SINGLE_TEAM = "teams/{id}";
+    String SINGLE_COMPETITION = "competitions/{id}/teams";
 }
--- src/test/java/hu/tmx/rest_assured_basic/FootballDataTest.java
+++ src/test/java/hu/tmx/rest_assured_basic/FootballDataTest.java
@@ -1,6 +1,7 @@
 package hu.tmx.rest_assured_basic;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
 
 import hu.tmx.config.Endpoints;
 import hu.tmx.config.FootballDataConfig;
@@ -13,6 +14,20 @@ public class FootballDataTest extends FootballDataConfig {
 
         given().queryParam("areas", 2072).
         when().get(Endpoints.SINGLE_AREA);
+    }
 
+    @Test
+    public void getDateTeamFounded(){
+
+        given().pathParam("id", 57).
+        when().get(Endpoints.SINGLE_TEAM).
+        then().body("founded", equalTo(1886));
+    }
+
+    @Test
+    public void getFirstTeamNameInGivenCompetition(){
+        given().pathParam("id", 2021).
+        when().get(Endpoints.SINGLE_COMPETITION).
+        then().body("teams.name[0]", equalTo("Arsenal FC"));
     }
 }
